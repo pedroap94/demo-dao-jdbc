ec2986b10bfa8b563e6c8ee8bf6c040b92bf2bf1
pedro <pedroalmodovar94@gmail.com>
2020-08-22 20:19:55 -0300

    Seller class added

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "books");
 
         System.out.println(dp);
+
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, dp);
+
+        System.out.println(seller);
     }
 }
